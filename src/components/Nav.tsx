370e187bcd9ff023df317e6b275a9f5b1d3f87f3
Hanqing1996 <zhq199622@163.com>
2020-07-03 16:14:46 +0800

    调整 Nav 样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,36 +1,39 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+import Icon from 'components/Icon';
 
-const NavWrapper = styled.nav`
-box-shadow: 0 0 3px rgba(0,0,0,0.25);
-flex-direction: row;
-line-height: 24px;
->ul{
-display: flex;
-flex-direction: row;
->li{
-width: 33.3%;
-text-align: center;
-padding: 16px;
-}
-}
+const NavWrapper = styled.ul`
+    display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    flex-direction: row;
+    font-size: 12px;
+`
+
+const NavItem = styled.li`
+    padding: 4px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `
 
 const Nav = () => {
     return (
         <NavWrapper>
-            <ul>
-                <li>
-                    <Link to='/tags'>标签</Link>
-                </li>
-                <li>
-                    <Link to='/money'>记账</Link>
-                </li>
-                <li>
-                    <Link to='/statistics'>统计</Link>
-                </li>
-            </ul>
+            <NavItem>
+                <Icon name="money"/>
+                <Link to='/money'>记账</Link>
+            </NavItem>
+            <NavItem>
+                <Icon name='labels'/>
+                <Link to='/tags'>标签</Link>
+            </NavItem>
+            <NavItem>
+                <Icon name="statistics"/>
+                <Link to='/statistics'>统计</Link>
+            </NavItem>
         </NavWrapper>
     )
 }
