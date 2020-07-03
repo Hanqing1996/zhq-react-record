be4b996a742b44d3c03d1814ae0a88928e744d90
Hanqing1996 <zhq199622@163.com>
2020-07-03 18:30:47 +0800

    创建 MoneyTags 组件

---
+++ src/components/money/MoneyTags.tsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import React, {Fragment} from "react";
+
+const TagsWrapper = styled.div`
+    background-color: white;
+    font-size: 14px;
+    padding: 8px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    flex-wrap: wrap;
+`
+
+const CurrentTags=styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    max-height: 12vh;
+    overflow: auto;
+    > li {
+        background: #d9d9d9;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 12px;
+        padding: 0 16px;
+        margin-right: 12px;
+        margin-top: 4px;
+    }
+`
+const NewTag=styled.div`
+    padding-top: 16px;
+    button {
+        background: transparent;
+        border: none;
+        color: #999;
+        border-bottom: 1px solid;
+        padding: 0 4px;
+    }
+`
+
+const MoneyTags = () => {
+
+    const tags = [
+        {id: 1, name: 'fuck'},
+        {id: 2, name: 'fuck2'},
+        {id: 3, name: 'fuck3'},
+        {id: 4, name: 'fuck4'},
+    ]
+
+    return (
+        <Fragment>
+            <TagsWrapper>
+                <CurrentTags>
+                    {tags.map(tag => <li
+                        key={tag.id}>
+                        {tag.name}
+                    </li>)}
+                </CurrentTags>
+
+            </TagsWrapper>
+            <NewTag>
+                <button>新增标签</button>
+            </NewTag>
+        </Fragment>
+
+    )
+}
+
+export default MoneyTags
+
+
+
+
